ebe25deb6a0dbaec6a419c0710ba5bf30b7da92c
jandrusak <jonathanandrusak@gmail.com>
2022-12-14 18:36:16 -0600

    update

--- scipts.js
+++ scipts.js
@@ -1,6 +1,6 @@
 const placeForResults = document.getElementById("results")
 
-new URLSearchParams(window.location.search).forEach((value, name) => {
-  placeForResults.append(`${name} : ${value}`)
-  placeForResults.append(document.createElement("br"))
-})
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+//   placeForResults.append(`${name} : ${value}`)
+//   placeForResults.append(document.createElement("br"))
+// })
